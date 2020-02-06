eae49c61bd46046a05e0c862d70005174ff5db8a
Sébastien Délèze <sebastien.deleze@rero.ch>
2020-02-06 13:42:34 +0100

    project: Configure translations
    * Configures translations for the project.
    * Add manual translations files
    * Closes #34.
    
    Co-Authored-by: Sébastien Délèze <sebastien.deleze@rero.ch>

--- projects/sonar/src/app/app.module.ts
+++ projects/sonar/src/app/app.module.ts
@@ -14,47 +14,46 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { RecordModule, CoreConfigService, TranslateLoader } from '@rero/ng-core';
-import { ToastrModule } from 'ngx-toastr';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { FormlyFieldConfig, FormlyModule } from '@ngx-formly/core';
+import { TranslateLoader as BaseTranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { CoreConfigService, RecordModule } from '@rero/ng-core';
+import { BsLocaleService } from 'ngx-bootstrap';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgxDropzoneModule } from 'ngx-dropzone';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { TabsModule } from 'ngx-bootstrap/tabs';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { TranslateModule, TranslateLoader as BaseTranslateLoader } from '@ngx-translate/core';
-import { FormlyModule, FormlyFieldConfig } from '@ngx-formly/core';
-import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
-import { BsLocaleService } from 'ngx-bootstrap';
-
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { ToastrModule } from 'ngx-toastr';
 import { AppConfigService } from './app-config.service';
-import { InstitutionComponent } from './record/institution/institution.component';
-import { DocumentComponent } from './record/document/document.component';
-import { DetailComponent as DocumentDetailComponent } from './record/document/detail/detail.component';
-import { UserComponent } from './record/user/user.component';
-import { DetailComponent as InstitutionDetailComponent } from './record/institution/detail/detail.component';
-import { DetailComponent as UserDetailComponent } from './record/user/detail/detail.component';
-import { JoinPipe } from './core/join.pipe';
-import { LanguageValuePipe } from './pipe/language-value.pipe';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UploadComponent } from './deposit/upload/upload.component';
-import { EditorComponent } from './deposit/editor/editor.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FileLinkPipe } from './core/file-link.pipe';
 import { FileSizePipe } from './core/filesize.pipe';
+import { HighlightJsonPipe } from './core/highlight-json.pipe';
+import { JoinPipe } from './core/join.pipe';
 import { StepComponent } from './core/step/step.component';
-import { ObjectTypeComponent } from './form/object-type.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BriefViewComponent } from './deposit/brief-view/brief-view.component';
+import { ConfirmationComponent } from './deposit/confirmation/confirmation.component';
+import { EditorComponent } from './deposit/editor/editor.component';
+import { ReviewComponent } from './deposit/review/review.component';
+import { UploadComponent } from './deposit/upload/upload.component';
 import { ArrayTypeComponent } from './form/array-type.component';
 import { FormlyHorizontalWrapperComponent } from './form/horizontal-wrapper.component';
-import { ConfirmationComponent } from './deposit/confirmation/confirmation.component';
-import { BriefViewComponent } from './deposit/brief-view/brief-view.component';
-import { FileLinkPipe } from './core/file-link.pipe';
-import { HighlightJsonPipe } from './core/highlight-json.pipe';
-import { ReviewComponent } from './deposit/review/review.component';
+import { ObjectTypeComponent } from './form/object-type.component';
+import { LanguageValuePipe } from './pipe/language-value.pipe';
+import { DetailComponent as DocumentDetailComponent } from './record/document/detail/detail.component';
+import { DocumentComponent } from './record/document/document.component';
+import { DetailComponent as InstitutionDetailComponent } from './record/institution/detail/detail.component';
+import { InstitutionComponent } from './record/institution/institution.component';
+import { DetailComponent as UserDetailComponent } from './record/user/detail/detail.component';
+import { UserComponent } from './record/user/user.component';
+import { TranslateLoader } from './translations/loader/translate-loader';
 import { AdminComponent } from './_layout/admin/admin.component';
 
 export function minElementError(err: any, field: FormlyFieldConfig) {
---
+++ projects/sonar/src/app/translations/manual_translations.ts
@@ -0,0 +1,61 @@
+/*
+ * SONAR UI
+ * Copyright (C) 2019 RERO
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { marker as _ } from '@biesbjerg/ngx-translate-extract-marker';
+
+// Institutions
+_('unifr');
+_('unisi');
+_('csal');
+
+// Provision activity
+_('bf:Publication');
+_('bf:Manufacture');
+_('bf:Distribution');
+_('bf:Production');
+_('bf:Place');
+
+// Document types
+_('type-book');
+_('type-journal');
+_('type-thesis');
+_('type-postprint');
+_('type-preprint');
+_('type-dissertation');
+_('type-report');
+_('type-newspaper');
+_('type-partition');
+_('type-newspapers');
+_('type-map');
+_('type-audio');
+_('type-print_media');
+_('type-issue');
+
+// Languages
+_('lang-eng');
+_('lang-fre');
+_('lang-ger');
+_('lang-ita');
+
+// Facets
+_('Institution');
+_('author__en');
+_('author__fr');
+_('author__de');
+_('author__it');
+_('language');
+_('subject');
