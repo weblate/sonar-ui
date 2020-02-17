c27e39d3c1e2fc7cca6902488c47ee20cfbd00f8
Sébastien Délèze <sebastien.deleze@rero.ch>
2020-02-17 15:47:25 +0100

    documents: add thumbnails in brief view
    * Adds thumbnails for documents in brief view.
    * Changes link to detail in brief view.
    * Adds label info of file in detail view.
    
    Co-Authored-by: Sébastien Délèze <sebastien.deleze@rero.ch>

--- projects/sonar/src/app/record/document/document.component.ts
+++ projects/sonar/src/app/record/document/document.component.ts
@@ -14,15 +14,43 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import { Component, Input } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 import { ResultItem } from '@rero/ng-core';
 
 @Component({
   templateUrl: './document.component.html'
 })
-export class DocumentComponent implements ResultItem {
+export class DocumentComponent implements ResultItem, OnInit {
+  // Record object
   record: any;
+
+  // Type of resource
   type: string;
+
+  // Detail URL object
   detailUrl: { link: string, external: boolean };
+
+  // Thumbnail file for record
+  thumbnail: any;
+
+  ngOnInit() {
+    this.loadThumbnail();
+  }
+
+  /**
+   * Load the thumbnail for record
+   */
+  private loadThumbnail() {
+    if (!this.record.metadata._files) {
+      return;
+    }
+
+    const thumbnails = this.record.metadata._files.filter((file: any) => file.type === 'thumbnail');
+
+    if (!thumbnails) {
+      return;
+    }
+
+    this.thumbnail = `/documents/${this.record.metadata.pid}/files/${thumbnails[0].key}`;
+  }
 }
