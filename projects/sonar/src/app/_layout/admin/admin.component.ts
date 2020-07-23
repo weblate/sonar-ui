c2d8a500d3caad874b77146d28eff3b6ce956844
Sébastien Délèze <sebastien.deleze@rero.ch>
2020-07-23 11:40:16 +0200

    interface: add language switcher and footer
    * Adds a footer in admin interface.
    * Adds a language switcher in admin interface.
    * Removes container from default layout.
    * Change from `div` to `ng-container` for showing HTML with condition.
    * Adds icon for logged user.
    * Closes #113.
    
    Co-Authored-by: Sébastien Délèze <sebastien.deleze@rero.ch>

--- projects/sonar/src/app/_layout/admin/admin.component.ts
+++ projects/sonar/src/app/_layout/admin/admin.component.ts
@@ -15,13 +15,15 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TranslateService } from '@rero/ng-core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Subscription } from 'rxjs';
+import { AppConfigService } from '../../app-config.service';
 import { UserService } from '../../user.service';
 
 @Component({
   selector: 'sonar-layout-admin',
-  templateUrl: './admin.component.html'
+  templateUrl: './admin.component.html',
 })
 export class AdminComponent implements OnInit, OnDestroy {
   // Logged user
@@ -41,8 +43,28 @@ export class AdminComponent implements OnInit, OnDestroy {
    *
    * @param _spinner Spinner service.
    * @param _userService User service.
+   * @param _configService Config service.
+   * @param _translateService: Translate service.
    */
-  constructor(private _spinner: NgxSpinnerService, private _userService: UserService) { }
+  constructor(
+    private _spinner: NgxSpinnerService,
+    private _userService: UserService,
+    private _configService: AppConfigService,
+    private _translateService: TranslateService
+  ) {}
+
+  get currentLanguage(): string {
+    return this._translateService.currentLanguage;
+  }
+
+  /**
+   * Return the list of available languages.
+   *
+   * @returns List of languages.
+   */
+  get languages(): Array<{ code: string; bibCode: string }> {
+    return this._configService.languagesMap;
+  }
 
   /**
    * Component initialization.
@@ -59,7 +81,6 @@ export class AdminComponent implements OnInit, OnDestroy {
           this._spinner.hide();
           this.ready = true;
         }
-
       },
       () => {
         this._spinner.hide();
@@ -73,5 +94,14 @@ export class AdminComponent implements OnInit, OnDestroy {
    */
   ngOnDestroy() {
     this._userSubscription.unsubscribe();
+  }
+
+  /**
+   * Change language.
+   *
+   * @param languageCode 2 digit language code.
+   */
+  changeLanguage(languageCode: string) {
+    this._translateService.setLanguage(languageCode);
   }
 }
